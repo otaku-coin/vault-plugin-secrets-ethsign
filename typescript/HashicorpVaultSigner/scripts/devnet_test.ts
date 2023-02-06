3223e9fb1723b0548ed112a3c44eb5212e985abf
wagyu298 <1514790+wagyu298@users.noreply.github.com>
2023-02-06 17:12:10 +0900

    Remove unused `fs` module

--- typescript/HashicorpVaultSigner/scripts/devnet_test.ts
+++ typescript/HashicorpVaultSigner/scripts/devnet_test.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import fs from 'node:fs/promises';
 import path from 'path';
 import {strict as assert} from 'assert';
 import process from 'process';
